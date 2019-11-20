35455e295ed369f397edcb4732a77c3f6bfb7fd5
John O'Sullivan <john@eximchain.com>
2019-11-20 17:27:12 -0500

    Hooked up data loading on detail page

--- src/pages/DeployDetail.tsx
+++ src/pages/DeployDetail.tsx
@@ -2,9 +2,10 @@ import React, { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from '@reach/router';
 import JSONPretty from 'react-json-pretty';
-import { AppState, DeploySelectors } from '../state';
+import { AppState, DeploySelectors, DeployActions } from '../state';
 import { DeployItem } from '@eximchain/ipfs-ens-types/spec/deployment';
 import { Box, Button } from '../components/sharedUI';
+import { AsyncDispatch } from '../state/sharedTypes';
 
 export interface DeployDetailPageProps extends RouteComponentProps {
   deployName?: string
@@ -12,13 +13,26 @@ export interface DeployDetailPageProps extends RouteComponentProps {
 
 interface StateProps {
   deploy: DeployItem | null
+  loading: boolean
+  error: any
 }
 
-const DeployDetailPage: FC<DeployDetailPageProps & StateProps> = (props) => {
-  const { deployName, deploy, navigate } = props;
+interface ActionProps {
+  fetchDeploy: (name:string) => void
+}
+
+const DeployDetailPage: FC<DeployDetailPageProps & StateProps & ActionProps> = (props) => {
+  const { deployName, deploy, navigate, error, loading, fetchDeploy } = props;
+
+  useEffect(function loadDeployIfMissing(){
+    if (!deployName) return;
+    if (loading || error || deploy) return;
+    fetchDeploy(deployName)
+  }, [loading, error, deploy, deployName, fetchDeploy])
 
   if (!deployName) return <p>No deployName in URL query.</p>
   if (!deploy) return <p>No deploy found in state.</p>
+
   return (
     <Box>
       <h2>{deployName} Details</h2>
@@ -31,8 +45,16 @@ const DeployDetailPage: FC<DeployDetailPageProps & StateProps> = (props) => {
 const mapStateToProps = (state: AppState, ownProps: DeployDetailPageProps) => {
   const { deployName } = ownProps;
   return {
-    deploy: deployName ? DeploySelectors.getDeploy(deployName)(state) : null
+    deploy: deployName ? DeploySelectors.getDeploy(deployName)(state) : null,
+    loading: DeploySelectors.isLoading.deploys(state),
+    error: state.deploy.error
   }
 }
 
-export default connect(mapStateToProps)(DeployDetailPage);
+const mapDispatchToProps = (dispatch:AsyncDispatch) => {
+  return {
+    fetchDeploy: (name:string) => dispatch(DeployActions.fetchDeploy(name))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeployDetailPage);
